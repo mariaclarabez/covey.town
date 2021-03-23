7b0316fdb74b9f1265f56913999f34d9ee5f6beb
Clara <soaresbezerra.m@northeastern.edu>
2021-03-22 22:04:30 -0400

    update database service to use remote heroku database

--- services/roomService/src/database/databaseService.ts
+++ services/roomService/src/database/databaseService.ts
@@ -3,10 +3,10 @@ const knex = require('knex')({
     client: 'pg',
     version: '13.2',
     connection: {
-      host : '127.0.0.1',
-      user : 'postgres',
-      password : 'cows',
-      database : 'covey.town'
+      host : 'ec2-3-91-127-228.compute-1.amazonaws.com',
+      user : 'ciwjmrzhmxcnet',
+      password : '977f88be1657c4bb2d8550b52244988a7af11963160ff4e83506b7a52564682f',
+      database : 'd314d8k6ng2vrk'
     }
   });
 
